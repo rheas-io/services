905a0ff74bf77aa60838a811a17f6ac357c857f3
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-02 00:23:49 +0530

    removed build dir from git

--- build/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { DriverManager } from "./driverManager";
-import { ServiceProvider } from "./serviceProvider";
-import { DeferredServiceProvider } from "./deferredServiceProvider";
-export { DriverManager, ServiceProvider, DeferredServiceProvider };
--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DeferredServiceProvider = exports.ServiceProvider = exports.DriverManager = void 0;
-const driverManager_1 = require("./driverManager");
-Object.defineProperty(exports, "DriverManager", { enumerable: true, get: function () { return driverManager_1.DriverManager; } });
-const serviceProvider_1 = require("./serviceProvider");
-Object.defineProperty(exports, "ServiceProvider", { enumerable: true, get: function () { return serviceProvider_1.ServiceProvider; } });
-const deferredServiceProvider_1 = require("./deferredServiceProvider");
-Object.defineProperty(exports, "DeferredServiceProvider", { enumerable: true, get: function () { return deferredServiceProvider_1.DeferredServiceProvider; } });
