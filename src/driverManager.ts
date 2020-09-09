a496802760395c0306772405a4b6b73bb7acd525
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-09 14:12:30 +0530

    activeDriver implemented

--- src/driverManager.ts
+++ src/driverManager.ts
@@ -11,6 +11,13 @@ export class DriverManager<T> implements IDriverManager<T> {
     protected _drivers: KeyValue<T> = {};
 
     /**
+     * Sets the active driver key/name.
+     *
+     * @var string
+     */
+    protected _activeDriver: string = '';
+
+    /**
      * Registers a driver instance to the given name. If force is false
      * and an instance already exists for the key, then it won't be replaced.
      *
@@ -25,12 +32,33 @@ export class DriverManager<T> implements IDriverManager<T> {
     }
 
     /**
-     * Returns the driver instance for the name.
+     * Sets the active driver name, if a driver of that name is registered.
+     * Otherwise an exception is thrown.
+     *
+     * Provide a second argument to forcefully register a driver on the given
+     * name and set it as active driver.
+     *
+     * @param name
+     * @param driver
+     */
+    public setActiveDriver(name: string, driver?: T): void {
+        if (driver) {
+            this.registerDriver(name, driver, true);
+        }
+        this.getDriver(name);
+
+        this._activeDriver = name;
+    }
+
+    /**
+     * Returns the driver instance for the name or returns the active driver,
+     * if no name is specified.
      *
      * @param name
      */
-    public getDriver(name: string): T {
-        const driverInstance = this._drivers[name];
+    public getDriver(name?: string): T {
+        const driverInstance =
+            name != null ? this._drivers[name] : this._drivers[this._activeDriver];
 
         if (!driverInstance) {
             throw new InvalidArgumentException(`Driver ${name} is not registered.`);
